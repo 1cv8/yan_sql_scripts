3ef59c7fe2a505efe5c96395751797921e0546b1
Сергей <1cv8ts@gmail.com>
2021-10-27 10:55:27 +0300

    compress_tables

---
+++ src/compress_tables.sql
@@ -0,0 +1,101 @@
+
+DECLARE @DBName VARCHAR(300) = '';
+DECLARE @TableName varchar(400) = '';
+
+DECLARE @SQLString NVARCHAR(MAX) = '';
+
+IF OBJECT_ID('tempdb..##ts_compress') IS NOT NULL
+	DROP TABLE ##ts_compress;
+
+DECLARE DBCursor CURSOR LOCAL STATIC FORWARD_ONLY READ_ONLY
+FOR (
+	SELECT * FROM 
+    (
+		SELECT TOP 100000
+			[name]
+		FROM sys.databases
+		WHERE
+			[name] not in ('master','tempdb','model','msdb')
+			AND [state_desc] = 'ONLINE'
+		ORDER BY
+			[name]
+	) as dbses
+	
+);
+
+OPEN DBCursor;
+FETCH NEXT FROM DBCursor INTO @DBName;
+WHILE @@FETCH_STATUS = 0
+    BEGIN
+		
+		SET @SQLString =
+		'USE [' + @DBName + ']
+			SELECT
+				SCHEMA_NAME(o.[schema_id]) as schema_name,
+				o.[name] as table_name
+			INTO ##ts_compress
+			FROM [' + @DBName + '].sys.objects as o
+			WHERE
+				o.object_id IN -- uncompressed tables
+				(
+					SELECT DISTINCT
+						[object_id]
+					FROM [' + @DBName + '].sys.partitions WITH(NOLOCK)
+					WHERE
+						[data_compression] = 0
+				)
+				AND o.[type] IN (''U'', ''V'') -- user types'
+
+		PRINT @DBName
+		--PRINT @SQLString
+		EXEC (@SQLString)
+
+		DECLARE TableCursor CURSOR FOR 
+		(
+			SELECT
+				'[' + @DBName + '].[' + tsc.[schema_name] + '].[' + tsc.[table_name]  + ']' as [table_name]
+			FROM ##ts_compress as tsc
+		);
+
+		-- Цикл по всем таблицам
+		OPEN TableCursor
+		FETCH NEXT FROM TableCursor INTO @TableName
+		WHILE @@FETCH_STATUS = 0
+		BEGIN
+			
+			
+			SET @SQLString = 'ALTER TABLE ' + @TableName + ' REBUILD WITH (DATA_COMPRESSION = PAGE, ONLINE = OFF, MAXDOP = 0);'--, SORT_IN_TEMPDB = ON);'
+				+ 'ALTER INDEX ALL ON ' + @TableName + ' REBUILD WITH (DATA_COMPRESSION = PAGE, ONLINE = OFF, MAXDOP = 0);'--, SORT_IN_TEMPDB = ON);'
+			
+			BEGIN TRAN
+
+			BEGIN TRY
+				
+				--PRINT @SQLString
+				EXEC(@SQLString)
+				COMMIT TRAN
+
+			END TRY
+			BEGIN CATCH
+				ROLLBACK TRAN
+				PRINT ('rolback: ' + @TableName);
+			END CATCH
+			
+			--PRINT '   tn:' + @TableName;
+			
+			-- Следующий элемент цикла по таблицам
+			FETCH NEXT FROM TableCursor INTO @TableName
+			
+		END
+
+		CLOSE TableCursor;
+		DEALLOCATE TableCursor;
+
+		DROP TABLE ##ts_compress;
+
+		-- Следующий элемент цикла по базам
+        FETCH NEXT FROM DBCursor INTO @DBName;
+    END
+
+CLOSE DBCursor;
+DEALLOCATE DBCursor;
